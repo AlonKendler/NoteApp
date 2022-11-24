d9f35c100e63c2b631c7ff4728d935bae2227265
alon kendler <alonkendler@gmail.com>
2022-11-24 12:30:39 +0200

    init vite ts code, adding bootsrap and react route

---
+++ Note-app/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import "bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>Hi</div>
+  )
+}
+
+export default App

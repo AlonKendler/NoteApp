893cdff353d7cc694a265d8527a22925e45aa9f9
alon kendler <alonkendler@gmail.com>
2022-11-24 12:44:13 +0200

    basic routing

--- Note-app/src/App.tsx
+++ Note-app/src/App.tsx
@@ -1,13 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import "bootstrap/dist/css/bootstrap.min.css"
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div>Hi</div>
-  )
+    <Container className="">
+      <Routes>
+        <Route path="/" element={<div>Hi</div>} />
+        <Route path="/new" element={<div>new</div>} />
+        <Route path="/:id">
+          <Route index element={<div>show</div>} />
+          <Route path="edit" element={<div>edit</div>} />
+        </Route>
+        <Route path="/*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
+  );
 }
 
-export default App
+export default App;
